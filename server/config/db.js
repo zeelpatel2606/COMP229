3e3522d168cf14b20606cf5aec7337a899802f6a
zeelpatel2606 <77913192+zeelpatel2606@users.noreply.github.com>
2022-10-28 15:04:11 -0400

    chnage db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost:27017/books229"
+  "URI": "mongodb+srv://zeel123:12345@cluster0.a2mhnx7.mongodb.net/?retryWrites=true&w=majority"
 };
